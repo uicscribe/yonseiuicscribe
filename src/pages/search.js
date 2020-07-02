f90cfdc84237d6bf9dd7ef2f8c752d6f50858559
Scribe <scribe.uic@gmail.com>
2020-07-02 10:48:21 +0900

    fix search

--- src/pages/search.js
+++ src/pages/search.js
@@ -9,7 +9,7 @@ import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 const searchClient = algoliasearch('L939SA528T', '6bf2bcd990937092c431629e2b44740d');
 const Hit = ({ hit }) =>
 <article>
-	<Link to={`/${hit.fields.slug}`}>
+	<Link to={`${hit.fields.slug}`}>
 		<h1>{hit.title}</h1>
 		<h2>{hit.date}{" "}by{" "}<span>{hit.author}</span>{" "}</h2>
 		<p>{hit.excerpt}</p>
